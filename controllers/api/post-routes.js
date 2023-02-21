63902c809b59c79ae45faf31eca51cca025b6e70
cadienlake <115520010+cadienlake@users.noreply.github.com>
2023-02-21 16:38:46 -0600

    update post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const Post = require("../../models/Post");
-const withAuth = require('../../utils/auth');
+const { Post, Comment, User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-
-
+// Make a blog post to home page
 router.post("/", async (req, res) => {
   try {
     const postData = await Post.create({
@@ -14,6 +13,58 @@ router.post("/", async (req, res) => {
     res.status(200).json(postData);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+// Post Comment on individual post page
+router.post("/:id", async (req, res) => {
+  try {
+    const postData = await Comment.create({
+      body: req.body.body,
+      user_id: req.session.user_id,
+      post_id: req.params.id,
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete post
+router.delete("/", async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!postData) {
+      res.status(404).json({ message: "No post found." });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update Post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.update(
+      {
+        title: req.body.title,
+        body: req.body.body,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(post);``
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
