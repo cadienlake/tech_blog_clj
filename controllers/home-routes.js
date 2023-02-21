559ddbfadb7a3e65b2e4a8d9f531d3dc39430d37
cadienlake <115520010+cadienlake@users.noreply.github.com>
2023-02-21 16:50:21 -0600

    Updated home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../models");
-const Post = require("../models/Post");
+const { Post, Comment, User } = require("../models");
 const withAuth = require("./../utils/auth");
 
 router.get("/", async (req, res) => {
@@ -20,14 +19,51 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get("/dashboard", (req, res) => {
-  if (req.session.loggedIn) {
-    res.render("dashboard");
-
-    return;
+// Find all posts made by user and display on dashboard if logged in
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [User],
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    if (req.session.loggedIn) {
+      res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
+      return;
+    }
+  } catch (err) {
+    res.json(err);
   }
-
   res.redirect("/");
+});
+
+// Find posts and relevant user/comment data by id
+router.get("/post/:id", withAuth, async (req, res) => {
+  // Retrieve posts by their primary key, and display with the user who made post
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    const post = postData.get({ plain: true });
+    // Retrieve comments that are linked to the post's id and display with User
+    const commentData = await Comment.findAll({
+      include: [User],
+      where: {
+        post_id: req.params.id,
+      },
+    });
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+    res.render("post", { post, comments, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
